29f0d834f892d208f86e23217c144a48761b3d14
Apple <apple@Apples-MacBook-Pro-98.local>
2019-08-01 11:33:39 -0700

    Iteration 1 IT WORKS

---
+++ Pokédex/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Apple on 7/31/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "BoxForest")!)
+        // Do any additional setup after loading the view.
+      
+  
+   // func tappedImage(sender: AnyObject) {
+}
+
+
+
+}
